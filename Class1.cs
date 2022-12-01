c51725ee9f1006365113196ea91c4739e1e7acdd
denis <106389581+fishman123456@users.noreply.github.com>
2022-12-01 13:43:08 +0700

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿[Serializable]
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+using static System.Console;
+
+Person person = new Person("Tom", 29);
+            Person person2 = new Person("Bill", 25);
+            Person[] people = new Person[] { person, person2 };
+
+// создаем объект SoapFormatter
+BinaryFormatter binFormat =
+new BinaryFormatter();
+
+// получаем поток, куда будем записывать сериализованный объект
+using (FileStream fs = new FileStream("people.bin", FileMode.OpenOrCreate))
+            {
+    binFormat.Serialize(fs, people);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            // десериализация
+            using (FileStream fs = new FileStream("people.bin", FileMode.OpenOrCreate))
+            {
+                Person[] newPeople = (Person[])binFormat.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован");
+                foreach (Person p in newPeople)
+                {
+                    Console.WriteLine("Имя: {0} --- Возраст: {1}", p.Name, p.Age);
+                }
+            }
+            Console.ReadLine();
+        
